c2fc9bf89964c3baf181489b4caa7361cdd5624e
Pranav Anilkumar <80296384+pranavanil47@users.noreply.github.com>
2024-02-08 16:06:36 +0400

    pranav

--- Group7-activity1.py
+++ Group7-activity1.py
@@ -6,3 +6,23 @@ def setPos(turta, x, y):
     t.penup()
     t.goto(turtle, x, y)
     t.pendown()
+    
+
+
+def hexagon(turta, hexa_color, border_color):
+    turta.pendown()
+    turta.pencolor(border_color)
+    turta.fillcolor(hexa_color)
+    turta.begin_fill()
+    turta.forward(50)
+    turta.right(60)
+    turta.forward(50)
+    turta.right(60)
+    turta.forward(50)
+    turta.right(60)
+    turta.forward(50)
+    turta.right(60)
+    turta.forward(50)
+    turta.right(60)
+    turta.forward(50)
+    turta.end_fill()

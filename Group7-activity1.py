0f0000b1c1dabb8b83791d6ffb9c3b1a993134bc
MohameddKD <mohammed.kdb@yahoo.com>
2024-02-08 17:03:59 +0400

    activity

--- Group7-activity1.py
+++ Group7-activity1.py
@@ -8,10 +8,11 @@ def setPos(turta, x, y):
     turta.pendown()
 
 def hexagon(turta, hexa_color, border_color):
+    
     turta.pendown()
     turta.pencolor(border_color)
     turta.fillcolor(hexa_color)
-    turta.begin_fill()
+    turta.begin_fill() 
     turta.forward(50)
     turta.right(60)
     turta.forward(50)
@@ -38,6 +39,15 @@ def square(turta, square_color,border_color):
     turta.forward(90)
     turta.right(90)
     turta.forward(90)
+    turta.end_fill()
+    turta.penup()
+
+def circle(turta, circle_color,border_color):
+    turta.pendown()
+    turta.pencolor(border_color)
+    turta.fillcolor(circle_color)
+    turta.begin_fill()
+    turta.circle(50)
     turta.end_fill()
     turta.penup()
 
